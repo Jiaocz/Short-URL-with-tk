96fd1bf6a2d3eeddaf0babdcb16d81f94668e90e
Orangii <14857984+Jiaocz@users.noreply.github.com>
2021-07-30 22:58:24 +0800

    init commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// 是否加载了配置
+define('CONFIG_LOADED', true);
+
+// 站点是否开启
+define('SITE_ENABLED', true);
+
+// GET方式是否开启
+define('API_GET_ENABLED', true);
+
+// 账户信息
+define('ACCOUNT_EMAIL', 'Your Email');
+define('ACCOUNT_PASSWORLD', 'Your Password');
+?>
